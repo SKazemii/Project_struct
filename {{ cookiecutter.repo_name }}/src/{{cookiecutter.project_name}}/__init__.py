669835dd5d7d054ec8fd12bd56617801cf83eff4
Saeed <62684826+SKazemii@users.noreply.github.com>
2024-02-08 00:31:21 -0400

    Update requirements.txt and logger configuration

--- {{ cookiecutter.repo_name }}/src/{{cookiecutter.project_name}}/__init__.py
+++ {{ cookiecutter.repo_name }}/src/{{cookiecutter.project_name}}/__init__.py
@@ -1,26 +1,21 @@
 import os
-import sys
 import logging
-import pathlib as Pathlb
+from {{cookiecutter.project_name}}.constants import LOGS_FILE_PATH
 
 
 
 
 def create_logger(level):
-    loggerName = Pathlb(__file__).stem
-    Pathlb(log_path).mkdir(parents=True, exist_ok=True)
-    grey = '\x1b[38;21m'
     blue = '\x1b[38;5;39m'
     yellow = '\x1b[38;5;226m'
-    red = '\x1b[38;5;196m'
     bold_red = '\x1b[31;1m'
     reset = '\x1b[0m'
 
-    logger = logging.getLogger(loggerName)
+    logger = logging.getLogger()
     logger.setLevel(level)
-    formatter_colored = logging.Formatter(blue + '[%(asctime)s]-' + yellow + '[%(name)s @%(lineno)d]' + reset + blue + '-[%(levelname)s]' + reset + bold_red + '\t\t%(message)s' + reset, datefmt='%m/%d/%Y %I:%M:%S %p ')
-    formatter = logging.Formatter('[%(asctime)s]-[%(name)s @%(lineno)d]-[%(levelname)s]\t\t%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p ')
-    file_handler = logging.FileHandler( os.path.join(log_path, loggerName + '_loger.log'), mode = 'w')
+    formatter_colored = logging.Formatter(blue + '[%(asctime)s]-' + yellow + '[%(module)s @%(lineno)d]' + reset + blue + '-[%(levelname)s]' + reset + bold_red + '\t\t%(message)s' + reset, datefmt='%m/%d/%Y %I:%M:%S %p ')
+    formatter = logging.Formatter('[%(asctime)s]-[%(module)s @%(lineno)d]-[%(levelname)s]\t\t%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p ')
+    file_handler = logging.FileHandler( os.path.join(LOGS_FILE_PATH, 'running_loger.log'), mode = 'w')
     file_handler.setLevel(level)
     file_handler.setFormatter(formatter)
     stream_handler = logging.StreamHandler()
